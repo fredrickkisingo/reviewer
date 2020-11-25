2d05cfb128b06ff49a6b6f76e569bb6d7cfa6ba0
fred <fredrickkisingo@gmail.com>
2020-11-25 18:11:52 +0300

    texts

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const reviews = [
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
     text:
-      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+      "Hello i really enjoyed this service!",
   },
   {
     id: 2,
@@ -16,7 +16,7 @@ const reviews = [
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
     text:
-      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+      "I highly reccomend this to anyone who wants to give a shot at web development",
   },
   {
     id: 3,
@@ -25,7 +25,7 @@ const reviews = [
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
     text:
-      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+      "I find internship a very pivotal stage in my career and using this site enabled me to gain more insights on interning",
   },
   {
     id: 4,
@@ -34,7 +34,7 @@ const reviews = [
     img:
       "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
     text:
-      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+      "I found this particular service very efficient to use and i willl highly reccomend it to any user who wants to give a shot at web development ",
   },
 ];
 
